2f10640bd2d63d189b13981fa8ee0ea1e24e8845
PiwanSama <piwan.summerlee@gmail.com>
2020-08-02 19:30:05 +0300

    Implemented user login

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
 <template>
   <div id="app">
     <Navigation />
-    <router-view class="container"/>
+    <router-view class="container" :user="user" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Navigation from "@/components/Navigation.vue";
+import db from "./db.js";
+//import firebase from "firebase";
 
 export default {
-   name: "app",
-   components: {
-      Navigation
+  name: "app",
+  data: function() {
+    return {
+      user: null
+    };
+  },
+  mounted() {
+    db.collection("users")
+      .doc("AUTrb0BUjcmmlakjtgvc")
+      .get()
+      .then(snapshot => {
+        this.user = snapshot.data().name;
+      });
+  },
+  components: {
+    Navigation
   }
-}
-
+};
 </script>
 <style lang="scss">
 $primary: #172a5a;
-@import "node_modules/bootstrap/scss/bootstrap"
+@import "node_modules/bootstrap/scss/bootstrap";
 </style>
