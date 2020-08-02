7392ca3df7724b72990c06645ca4c6ecc511dbc1
PiwanSama <piwan.summerlee@gmail.com>
2020-08-02 18:28:53 +0300

    Added firebase connection to application

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
   <div id="app">
+    <Navigation />
     <router-view class="container"/>
   </div>
 </template>
 
+<script>
+// @ is an alias to /src
+import Navigation from "@/components/Navigation.vue";
+
+export default {
+   name: "app",
+   components: {
+      Navigation
+  }
+}
+
+</script>
 <style lang="scss">
+$primary: #172a5a;
 @import "node_modules/bootstrap/scss/bootstrap"
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,3 +1,14 @@
 <template>
-    <div>Navigation</div>
+    <div>
+        <nav class="site-nav navbar navbar-expand bg-primary navbar-dark">
+            <div class="container-fluid">
+                <router-link class="navbar-brand" to="/">Meeting Log</router-link>
+                <div class="navbar-nav ml-auto">
+                    <router-link class="nav-item nav-link" to="/meetings">Meetings</router-link>
+                    <router-link class="nav-item nav-link" to="/login">Login</router-link>
+                    <router-link class="nav-item nav-link" to="/register">Register</router-link>
+                </div>
+            </div>
+        </nav>
+    </div>
 </template>
